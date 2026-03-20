9ee5125b8352a2aa5dcc6f865a73b57ac4644383
mathbrlima <mathbrlima2007@gmail.com>
2026-03-19 22:28:47 -0300

    aula 2 variáveis e operadores

---
+++ aula02-variaveis-operadores/aula02a-variaveis.py
@@ -0,0 +1,28 @@
+print("ola mundo")
+
+print(7 + 4)
+print("7 + 4")
+print("7" + "4") # CONCATENAÇÂO DE STRINGS
+
+# Comentários de 1 linha
+'''
+    Comentários de 
+    múltiplas linhas
+'''
+
+# VARIÁVEIS
+nome = "Matheus" # str
+idade = 26 # int
+peso = 70.2 # float
+
+print(nome, idade, peso)
+print(f"Oiii, {nome}!!!!")
+
+# INPUTS - SIMULAÇÂO DE FORMS NO CMD
+nome = input("Disite o seu nome: ")
+idade = int(input("Digite sua idade: "))
+
+nova_idade = idade + 1
+
+print(nome, idade)
+print(nova_idade)
