1cae9f48a7b86412a41efec882ec5155de2f2644
Matheus <mathbrlima2007@gmail.com>
2026-05-03 22:54:48 -0300

    resolução de alguns exercícios

---
+++ Aula05-strings_arrays_matrizes/exercicio3.py
@@ -0,0 +1,8 @@
+meses = ["Jan", "Fev", "Mar", "Abr", "Mai", "Jun",
+         "Jul", "Ago", "Set", "Out", "Nov", "Dez"]
+
+dias = [31, 28, 31, 30, 31, 30,
+        31, 31, 30, 31, 30, 31]
+
+for i in range(12):
+    print(f"O mês de {meses[i]} tem {dias[i]} dias ao todo.")
